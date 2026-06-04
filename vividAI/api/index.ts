e8dd66463dd2770f55b3b2c0eb6780f8439bbf87
itzzSVRtech <itzzsvrtech@itzzSVRtechs-MacBook-Air.local>
2026-06-04 14:34:31 +0530

    vividAI Deployment Fixed

---
+++ vividAI/api/index.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import dotenv from "dotenv";
+import { apiRouter } from "../src/apiRouter";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+// Mount the API router
+app.use("/api", apiRouter);
+
+export default app;
